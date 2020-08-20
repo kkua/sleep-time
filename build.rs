4f1d7c757c96ab44f3e399718ee0894609ea3f3b
KK <kk-pub@outlook.com>
2020-08-20 22:40:54 +0800

    chore: 升级依赖版本

--- build.rs
+++ build.rs
@@ -1,4 +1,12 @@
 fn main() {
     #[cfg(windows)]
-    embed_resource::compile("resources/app.rc");
+    {
+        let target = std::env::var("TARGET").unwrap();
+        if let Some(tool) = cc::windows_registry::find_tool(target.as_str(), "cl.exe") {
+            for (key, value) in tool.env() {
+                std::env::set_var(key, value);
+            }
+        }
+        embed_resource::compile("resources/app.rc");
+    }
 }
